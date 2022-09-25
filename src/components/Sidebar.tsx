7fd60fd684498fa4874b14164a721e1c7d405432
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-25 18:48:54 +0100

    feat: add logo to Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,9 @@
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <span className="w-full h-14 object-contain text-white">SPLOTIFY</span>
+    </div>
   )
 }
 
