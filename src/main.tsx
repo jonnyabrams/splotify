69a0cbaa741c5d54f1b1c5c7bd501fa7402bcbd8
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-26 11:32:57 +0100

    feat: redux store & playerSlice

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import "./index.css";
+const { store } = require('./redux/store'); 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
