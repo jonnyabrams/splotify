75bc99137b3ff43f5cd6538f0eccb410c4bcf2d8
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-26 14:04:41 +0100

    feat: Loader and Error components

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -8,6 +8,10 @@ const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
 
+  if (isFetching) return <Loader title="Loading songs..." />
+
+  if (error) return <Error />
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
