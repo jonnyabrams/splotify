3a21f6200d6df88e03e0e9e4c3fec7da7dd76a03
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-26 11:07:50 +0100

    feat: Discover page initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Sidebar, Searchbar } from "./components";
+import { Sidebar, Searchbar, TopPlay } from "./components";
 import {
   ArtistDetails,
   TopArtists,
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div>
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         <Searchbar />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
@@ -29,6 +29,9 @@ const App = () => {
               <Route path="/songs/:songid" element={<SongDetails />} />
               <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
           </div>
         </div>
       </div>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -6,7 +6,7 @@ import ArtistCard from "./ArtistCard";
 import DetailsHeader from "./DetailsHeader";
 import SongBar from "./SongBar";
 import RelatedSongs from "./RelatedSongs";
-import MusicPlayer from "./MusicPlayer";
+// import MusicPlayer from "./MusicPlayer";
 import Loader from "./Loader";
 import Error from "./Error";
 
@@ -19,7 +19,7 @@ export {
   DetailsHeader,
   SongBar,
   RelatedSongs,
-  MusicPlayer,
+  // MusicPlayer,
   Loader,
   Error,
 };
