d7f08469da0b1688cdf298d557fc9b9469621287
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-28 07:54:03 +0100

    feat: fetch songs by genre (for Discover selector)

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -3,13 +3,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 // @ts-ignore
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useGetSongsByGenreQuery } from "../redux/services/shazamCore";
 import { RootObject } from "../../types";
+// @ts-ignore
+import { selectGenreListId } from "../redux/features/playerSlice";
 
 const Discover = () => {
   const dispatch = useDispatch();
-  const { activeSong, isPlaying } = useSelector((state: any) => state.player);
-  const { data, isFetching, error } = useGetTopChartsQuery();
+  const { activeSong, isPlaying, genreListId } = useSelector(
+    (state: any) => state.player
+  );
+  const { data, isFetching, error } = useGetSongsByGenreQuery(
+    genreListId || "POP"
+  );
   const genreTitle = "Pop";
 
   if (isFetching) return <Loader title="Loading songs..." />;
@@ -23,8 +29,8 @@ const Discover = () => {
           Discover {genreTitle}
         </h2>
         <select
-          onChange={() => {}}
-          value=""
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || "pop"}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,6 +16,9 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({
       query: () => "/charts/world",
     }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/charts/genre-world?genre_code=${genre}`,
+    }),
     getSongDetails: builder.query({
       query: ({ songid }) => `/tracks/details?track_id=${songid}`,
     }),
@@ -33,6 +36,7 @@ export const shazamCoreApi = createApi({
 
 export const {
   useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
