010d31de719932d1ac7aedcda4155aaa46959d4b
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-28 08:03:53 +0100

    feat: enter search terms & navigate to Search page dynamically

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -3,8 +3,18 @@ import { useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 
 const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`)
+  };
+
   return (
     <form
+      onSubmit={handleSubmit}
       autoComplete="off"
       className="p-2 text-gray-400 focus-within:text-gray-600"
     >
@@ -19,8 +29,8 @@ const Searchbar = () => {
           id="search-field"
           placeholder="Search"
           type="search"
-          value=""
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
         />
       </div>
